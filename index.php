81412378b6e23e745c2b0021cd7112e3fced4d2f
thamrongrit53 <47946165+thamrongrit53@users.noreply.github.com>
2020-05-21 01:18:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,17 @@
 <?php
-   $accessToken = "B56+JXIosMFlP8aLyUw3hq1DEQGoEQcnKx3GKi0zhCwafgCyuiOWPiK/6SvUnMD1SCMnZIgVykAm+co7h7m4ZVTNukGksmfW7NMUaZkmtm4YkZX1PSlhMjjDyRkiyraahTkTKhp9yNaEgM8XKfbynAdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
-$content = file_get_contents('php://input');
-   $arrayJson = json_decode($content, true);
-$arrayHeader = array();
-   $arrayHeader[] = "Content-Type: application/json";
-   $arrayHeader[] = "Authorization: Bearer {$accessToken}";
 
-//    $message = $arrayJson['events'][0]['message']['text'];
-//    if(isset($arrayJson['events'][0]['source']['groupId'])){
-//       $id = $arrayJson['events'][0]['source']['groupId'];
-//    }
-//    else if(isset($arrayJson['events'][0]['source']['room'])){
-//       $id = $arrayJson['events'][0]['source']['room'];
-//    }
-$jsonFlex = 
+
+$API_URL = 'https://api.line.me/v2/bot/message';
+$ACCESS_TOKEN = 'B56+JXIosMFlP8aLyUw3hq1DEQGoEQcnKx3GKi0zhCwafgCyuiOWPiK/6SvUnMD1SCMnZIgVykAm+co7h7m4ZVTNukGksmfW7NMUaZkmtm4YkZX1PSlhMjjDyRkiyraahTkTKhp9yNaEgM8XKfbynAdB04t89/1O/w1cDnyilFU='; 
+$channelSecret = 'dbd3546739b8b6d643d1a5a702ea1251';
+
+
+$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+
+$request = file_get_contents('php://input');   // Get request content
+$request_array = json_decode($request, true);   // Decode JSON to Array
+
+$jsonFlex = [
     "type" => "flex",
     "altText" => "Hello Flex Message",
     "contents" => [
@@ -129,25 +127,51 @@ $jsonFlex =
           ]
         ]
       ]
-    ];
-      $arrayPostData['to'] = "C40a0888cfb8018cf6263bd8529e9828d";
-      $arrayPostData['messages'][0]['type'] =$jsonFlex;
-     
+    ]
+  ];
 
-      pushMsg($arrayHeader,$arrayPostData);
- 
-function pushMsg($arrayHeader,$arrayPostData){
-      $strUrl = "https://api.line.me/v2/bot/message/push";
-$ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL,$strUrl);
-      curl_setopt($ch, CURLOPT_HEADER, false);
-      curl_setopt($ch, CURLOPT_POST, true);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-      $result = curl_exec($ch);
-      curl_close ($ch);
-   }
-exit;
+
+
+if ( sizeof($request_array['events']) > 0 ) {
+    foreach ($request_array['events'] as $event) {
+        error_log(json_encode($event));
+        $reply_message = '';
+        $reply_token = $event['replyToken'];
+
+
+        $data = [
+            'replyToken' => $reply_token,
+            'messages' => [$jsonFlex]
+        ];
+
+        print_r($data);
+
+        $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+
+        $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+
+        echo "Result: ".$send_result."\r\n";
+        
+    }
+}
+
+echo "OK";
+
+
+
+
+function send_reply_message($url, $post_header, $post_body)
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+}
+
 ?>
