f2b3faff7f6c1d3600a9334bf88522aa96a88fa9
thamrongrit53 <47946165+thamrongrit53@users.noreply.github.com>
2020-05-26 00:24:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-   <title>line</title>
+	<title>Line broadcast</title>
     <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
@@ -10,23 +10,52 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 </head>
 <body>
-<div class="container" style="margin-top: 20px;">
-  <div class="row">
-  <div class="col-md-12">
-   <h1>line</h1>
-  <form method="POST" action="line.php?id=1">
-    <input class="form-control" type="text" name="text">  
-     <br>
-     <button type="submit" value="Submit" class="btn btn-danger">
-        send text
-</button>
-  </form>
-     <br>
-<button type="button" class="btn btn-warning"><a href="https://p888999p.herokuapp.com/line.php?id=2">send flex messages</a>
-</button>
-<!--   <button type="button" class="btn btn-dark"></button></div> -->
-  </div>
-</div>
+<div class="container">
+  <h2>Line broadcast System</h2>
+  <!-- Button to Open the Modal -->
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+  	Send Text
+  </button>
 
+  <!-- The Modal -->
+  <div class="modal fade" id="myModal">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Send Text</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        <!-- Modal body -->
+        <div class="modal-body">
+		<form method="POST" action="line.php?id=1">
+    		<input class="form-control" type="text" name="text">  
+     		<br>
+     		<button type="submit" value="Submit" class="btn btn-secondary">
+        Send Text
+			</button>
+  			</form>        
+  		</div>
+        
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+  
+
+
+
+
+
+
+
+
+  
+</div>
 </body>
 </html>
