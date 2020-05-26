fdfd3852ea762afc94494913d6b576c95cae4be3
thamrongrit53 <47946165+thamrongrit53@users.noreply.github.com>
2020-05-26 23:11:34 +0700

    Update line.php

--- line.php
+++ line.php
@@ -35,215 +35,152 @@ $jsonFlex ='{"to": "C40a0888cfb8018cf6263bd8529e9828d",
       "altText": "Flex Message",
       "contents": {
   "type": "bubble",
+  "hero": {
+    "type": "image",
+    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+    "size": "full",
+    "aspectRatio": "20:13",
+    "aspectMode": "cover",
+    "action": {
+      "type": "uri",
+      "uri": "http://linecorp.com/"
+    }
+  },
   "body": {
     "type": "box",
     "layout": "vertical",
     "contents": [
       {
         "type": "text",
-        "text": "RECEIPT",
+        "text": "Brown Cafe",
         "weight": "bold",
-        "color": "#1DB446",
-        "size": "sm"
-      },
-      {
-        "type": "text",
-        "text": "Brown Store",
-        "weight": "bold",
-        "size": "xxl",
-        "margin": "md"
-      },
-      {
-        "type": "text",
-        "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
-        "size": "xs",
-        "color": "#aaaaaa",
-        "wrap": true
-      },
-      {
-        "type": "separator",
-        "margin": "xxl"
+        "size": "xl"
       },
       {
         "type": "box",
-        "layout": "vertical",
-        "margin": "xxl",
-        "spacing": "sm",
+        "layout": "baseline",
+        "margin": "md",
         "contents": [
           {
-            "type": "box",
-            "layout": "horizontal",
-            "contents": [
-              {
-                "type": "text",
-                "text": "Energy Drink",
-                "size": "sm",
-                "color": "#555555",
-                "flex": 0
-              },
-              {
-                "type": "text",
-                "text": "$2.99",
-                "size": "sm",
-                "color": "#111111",
-                "align": "end"
-              }
-            ]
+            "type": "icon",
+            "size": "sm",
+            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
           },
           {
-            "type": "box",
-            "layout": "horizontal",
-            "contents": [
-              {
-                "type": "text",
-                "text": "Chewing Gum",
-                "size": "sm",
-                "color": "#555555",
-                "flex": 0
-              },
-              {
-                "type": "text",
-                "text": "$0.99",
-                "size": "sm",
-                "color": "#111111",
-                "align": "end"
-              }
-            ]
+            "type": "icon",
+            "size": "sm",
+            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
           },
           {
-            "type": "box",
-            "layout": "horizontal",
-            "contents": [
-              {
-                "type": "text",
-                "text": "Bottled Water",
-                "size": "sm",
-                "color": "#555555",
-                "flex": 0
-              },
-              {
-                "type": "text",
-                "text": "$3.33",
-                "size": "sm",
-                "color": "#111111",
-                "align": "end"
-              }
-            ]
+            "type": "icon",
+            "size": "sm",
+            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
           },
           {
-            "type": "separator",
-            "margin": "xxl"
+            "type": "icon",
+            "size": "sm",
+            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
           },
           {
-            "type": "box",
-            "layout": "horizontal",
-            "margin": "xxl",
-            "contents": [
-              {
-                "type": "text",
-                "text": "ITEMS",
-                "size": "sm",
-                "color": "#555555"
-              },
-              {
-                "type": "text",
-                "text": "3",
-                "size": "sm",
-                "color": "#111111",
-                "align": "end"
-              }
-            ]
+            "type": "icon",
+            "size": "sm",
+            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
           },
           {
-            "type": "box",
-            "layout": "horizontal",
-            "contents": [
-              {
-                "type": "text",
-                "text": "TOTAL",
-                "size": "sm",
-                "color": "#555555"
-              },
-              {
-                "type": "text",
-                "text": "$7.31",
-                "size": "sm",
-                "color": "#111111",
-                "align": "end"
-              }
-            ]
-          },
-          {
-            "type": "box",
-            "layout": "horizontal",
-            "contents": [
-              {
-                "type": "text",
-                "text": "CASH",
-                "size": "sm",
-                "color": "#555555"
-              },
-              {
-                "type": "text",
-                "text": "$8.0",
-                "size": "sm",
-                "color": "#111111",
-                "align": "end"
-              }
-            ]
-          },
-          {
-            "type": "box",
-            "layout": "horizontal",
-            "contents": [
-              {
-                "type": "text",
-                "text": "CHANGE",
-                "size": "sm",
-                "color": "#555555"
-              },
-              {
-                "type": "text",
-                "text": "$0.69",
-                "size": "sm",
-                "color": "#111111",
-                "align": "end"
-              }
-            ]
+            "type": "text",
+            "text": "4.0",
+            "size": "sm",
+            "color": "#999999",
+            "margin": "md",
+            "flex": 0
           }
         ]
       },
       {
-        "type": "separator",
-        "margin": "xxl"
-      },
-      {
         "type": "box",
-        "layout": "horizontal",
-        "margin": "md",
+        "layout": "vertical",
+        "margin": "lg",
+        "spacing": "sm",
         "contents": [
           {
-            "type": "text",
-            "text": "PAYMENT ID",
-            "size": "xs",
-            "color": "#aaaaaa",
-            "flex": 0
+            "type": "box",
+            "layout": "baseline",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "text",
+                "text": "Place",
+                "color": "#aaaaaa",
+                "size": "sm",
+                "flex": 1
+              },
+              {
+                "type": "text",
+                "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
+                "wrap": true,
+                "color": "#666666",
+                "size": "sm",
+                "flex": 5
+              }
+            ]
           },
           {
-            "type": "text",
-            "text": "#743289384279",
-            "color": "#aaaaaa",
-            "size": "xs",
-            "align": "end"
+            "type": "box",
+            "layout": "baseline",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "text",
+                "text": "Time",
+                "color": "#aaaaaa",
+                "size": "sm",
+                "flex": 1
+              },
+              {
+                "type": "text",
+                "text": "10:00 - 23:00",
+                "wrap": true,
+                "color": "#666666",
+                "size": "sm",
+                "flex": 5
+              }
+            ]
           }
         ]
       }
     ]
   },
-  "styles": {
-    "footer": {
-      "separator": true
-    }
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "spacing": "sm",
+    "contents": [
+      {
+        "type": "button",
+        "style": "link",
+        "height": "sm",
+        "action": {
+          "type": "uri",
+          "label": "CALL",
+          "uri": "https://linecorp.com"
+        }
+      },
+      {
+        "type": "button",
+        "style": "link",
+        "height": "sm",
+        "action": {
+          "type": "uri",
+          "label": "WEBSITE",
+          "uri": "https://linecorp.com"
+        }
+      },
+      {
+        "type": "spacer",
+        "size": "sm"
+      }
+    ],
+    "flex": 0
   }
 }
 }
